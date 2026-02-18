d264b32d539cf64ff08a0853a081fa1d19465fe0
Joao Pedro <joaopedro.lessa@outlook.com>
2026-02-18 11:18:27 -0300

    Initial commit: Telecom API Structure

---
+++ src/main/java/com/br/telecom/gerenciador_planos/GerenciadorPlanosApplication.java
@@ -0,0 +1,13 @@
+package com.br.telecom.gerenciador_planos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorPlanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorPlanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/telecom/gerenciador_planos/GerenciadorPlanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.telecom.gerenciador_planos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPlanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
